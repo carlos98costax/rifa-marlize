dfc946ae2f199434f952ba78eb17bd5a85e1bce7
carlos98costa <46066793+carlos98costa@users.noreply.github.com>
2025-06-14 00:58:58 -0300

    fix: remove unused INumber import to resolve TS6133

--- api/numbers.ts
+++ api/numbers.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import NumberModel, { INumber } from './models/Number.js';
+import NumberModel from './models/Number.js';
 
 dotenv.config();
 
